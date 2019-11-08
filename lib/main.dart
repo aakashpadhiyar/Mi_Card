330671b1af59180c1239f5ff50da357c1c2d786a
Aakash Padhiyar <aakashpadhiyar06@gmail.com>
2019-11-08 20:12:38 +0530

    challenge

--- lib/main.dart
+++ lib/main.dart
@@ -12,27 +12,33 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
-                height: 100.0,
+                height: double.infinity,
                 width: 100.0,
-                color: Colors.deepOrange,
+                color: Colors.red,
                 child: Text('container: 1'),
               ),
-              SizedBox(
-                width: 20.0,
-              ),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                      child: Text('container: 2'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.green,
+                    ),
+                  ]),
               Container(
                 width: 100.0,
-                height: 100.0,
-                color: Colors.white,
-                child: Text('container: 2'),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.green,
+                height: double.infinity,
+                color: Colors.blue,
                 child: Text('container: 3'),
               )
             ],
