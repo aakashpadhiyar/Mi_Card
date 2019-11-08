17bc87b489cbb07464b802d92c3bcae22d433d40
Aakash Padhiyar <aakashpadhiyar06@gmail.com>
2019-11-08 20:01:15 +0530

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.deepOrange,
+                child: Text('container: 1'),
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.white,
+                child: Text('container: 2'),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.green,
+                child: Text('container: 3'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
